beebfb5d37fdb3e614f60115255fcb74ef5bb393
Fernando Rocha <fernandorff92@gmail.com>
2023-12-03 13:28:09 -0300

    WIP - Cleaning code

--- product-service/src/main/resources/db/schema.sql
+++ product-service/src/main/resources/db/schema.sql
@@ -29,3 +29,4 @@ create table "product"."t_product" ("price" float(53), "id" bigserial not null, 
 create table "product"."t_product" ("price" float(53), "id" bigserial not null, "image_url" varchar(255), "name" varchar(255), primary key ("id"));
 create table "product"."t_product" ("price" float(53), "id" bigserial not null, "image_url" varchar(255), "name" varchar(255), primary key ("id"));
 create table "product"."t_product" ("price" float(53), "id" bigserial not null, "image_url" varchar(255), "name" varchar(255), primary key ("id"));
+create table "product"."t_product" ("price" float(53), "id" bigserial not null, "image_url" varchar(255), "name" varchar(255), primary key ("id"));
--- stock-service/src/main/resources/db/schema.sql
+++ stock-service/src/main/resources/db/schema.sql
@@ -68,3 +68,4 @@ create table "stock"."t_stock" ("available_amount" integer, "id" bigserial not n
 create table "stock"."t_stock" ("available_amount" integer, "id" bigserial not null, "product_id" bigint, "description" varchar(255), primary key ("id"));
 create table "stock"."t_stock" ("available_amount" integer, "id" bigserial not null, "product_id" bigint, "description" varchar(255), primary key ("id"));
 create table "stock"."t_stock" ("available_amount" integer, "id" bigserial not null, "product_id" bigint, "description" varchar(255), primary key ("id"));
+create table "stock"."t_stock" ("available_amount" integer, "id" bigserial not null, "product_id" bigint, "description" varchar(255), primary key ("id"));
